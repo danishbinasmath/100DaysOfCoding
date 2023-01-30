ee15ea3c799d667044a873861d8d8c0cf5f88681
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-01-30 19:23:38 +0530

    Add files via upload

---
+++ Day6/Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,18 @@
+package Day6;
+
+public class Delete_Node_in_a_Linked_List {
+
+//      Definition for singly-linked list.
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+        }
+
+    class Solution {
+        public void deleteNode(ListNode node) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
