57e53e5e3ace82a1179d9cfef34f685e8b89c272
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-02-07 23:36:53 +0530

    Add files via upload

---
+++ Day14/Maximum_Depth_of_Binary_Tree.java
@@ -0,0 +1,30 @@
+package Day14;
+
+public class Maximum_Depth_of_Binary_Tree {
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if(root == null) {
+                return 0;
+            }
+
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+
+            return Math.max(left, right) + 1;
+        }
+    }
+}
