995cc3408a61ab44bd3e3befb4be26f8876769a2
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-02-15 22:22:37 +0530

    Add files via upload

---
+++ Day22/Subsets.java
@@ -0,0 +1,22 @@
+package Day22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+    class Solution {
+        public List<List<Integer>> subsets(int[] nums) {
+            List<List<Integer>> outer = new ArrayList<>();
+            outer.add(new ArrayList<>());
+            for(int num: nums) {
+                int n = outer.size();
+                for(int i=0; i<n; i++){
+                    List<Integer> internal = new ArrayList<>(outer.get(i));
+                    internal.add(num);
+                    outer.add(internal);
+                }
+            }
+            return outer;
+        }
+    }
+}
