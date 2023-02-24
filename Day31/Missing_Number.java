84c7c7f3c80b11a1e9fd0b5db4708c2e365454e0
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-02-24 21:53:04 +0530

    Add files via upload

---
+++ Day31/Missing_Number.java
@@ -0,0 +1,36 @@
+package Day31;
+
+public class Missing_Number {
+
+    //Method 1
+    class Solution1 {
+        public int missingNumber(int[] nums) {
+            int n=nums.length,totalsum=n*(n+1)/2; //sum of first n natural numbers
+            for(int i:nums)
+                totalsum-=i;
+            return totalsum;
+        }
+    }
+
+//    Method 2
+    //rootThe basic idea is to use XOR operation.
+    //We all know that a^b^b =a, which means two xor operations
+    //with the same number will eliminate the number and reveal the original number.
+    //In this solution, I apply XOR operation to both the index and value of the array.
+    //In a complete array with no missing numbers, the index and value should
+    //be perfectly corresponding( nums[index] = index), so in a missing array,
+    //what left finally is the missing number.
+    class Solution2 {
+        public int missingNumber(int[] nums) {
+    
+            int xor = 0, i = 0;
+            for (i = 0; i < nums.length; i++) {
+                xor = xor ^ i ^ nums[i];
+            }
+
+            return xor ^ i;
+        }
+    }
+
+}
+
