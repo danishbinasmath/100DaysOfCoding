92d2035a2c6453287edb79389bf90f67b0a5c0c9
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-03-18 13:59:47 +0530

    Add files via upload

---
+++ Day53/Power_of_Two.java
@@ -0,0 +1,16 @@
+package Day53;
+
+public class Power_of_Two {
+    class Solution {
+        public boolean isPowerOfTwo(int n) {
+            // Handle edge cases
+            if (n == 0 || n == Integer.MIN_VALUE) {
+                return false;
+            }
+
+            //Just use '&' operator on the number(i.e n) with the number-1(i.e n-1),
+            //and if that is equal to 0 then it is a power of 2.
+            return (n & (n-1)) == 0;
+        }
+    }
+}
