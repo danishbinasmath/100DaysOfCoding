f46f5f70f5e655f3695e78c773d974d57f52b89f
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-04-29 23:54:50 +0530

    Add files via upload

---
+++ Day94/Unique_Number_of_Occurrences.java
@@ -0,0 +1,14 @@
+package Day94;
+
+import java.util.*;
+
+public class Unique_Number_of_Occurrences {
+    class Solution {
+        public boolean uniqueOccurrences(int[] arr) {
+            Map<Integer, Integer> count = new HashMap<>();
+            for (int a : arr)
+                count.put(a, 1 + count.getOrDefault(a, 0));
+            return count.size() == new HashSet<>(count.values()).size();
+        }
+    }
+}
