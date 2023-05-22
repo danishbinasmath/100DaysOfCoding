dffd26679dc5d29aae6cce5695dfae1989834357
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-05-22 17:44:57 +0530

    Add files via upload

---
+++ Day117/Top_K_Frequent_Elements.java
@@ -0,0 +1,26 @@
+package Day117;
+
+import java.util.*;
+
+public class Top_K_Frequent_Elements {
+    class Solution {
+        public int[] topKFrequent(int[] nums, int k) {
+
+            // Creating a Frequency Table
+            Map<Integer,Integer> map = new HashMap();
+            for(int n : nums){
+                map.put(n,map.getOrDefault(n,0) + 1);
+            }
+            // Putting values in priority queue
+            PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->map.get(b) - map.get(a));
+            pq.addAll(map.keySet());
+
+            // putting the top k values in array
+            int[] res = new int[k];
+            for(int i = 0;i<k;i++){
+                res[i] = pq.poll();
+            }
+            return res;
+        }
+    }
+}
