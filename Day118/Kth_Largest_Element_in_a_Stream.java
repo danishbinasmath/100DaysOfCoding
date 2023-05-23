60095e7ddd918fb0ede86e7a269362a3ee1c4e6a
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-05-23 17:41:10 +0530

    Add files via upload

---
+++ Day118/Kth_Largest_Element_in_a_Stream.java
@@ -0,0 +1,27 @@
+package Day118;
+
+import java.util.*;
+
+public class Kth_Largest_Element_in_a_Stream {
+    class KthLargest {
+        final PriorityQueue<Integer> q;
+        final int k;
+
+        public KthLargest(int k, int[] a) {
+            this.k = k;
+            q = new PriorityQueue<>(k);
+            for (int n : a)
+                add(n);
+        }
+
+        public int add(int n) {
+            if (q.size() < k)
+                q.offer(n);
+            else if (q.peek() < n) {
+                q.poll();
+                q.offer(n);
+            }
+            return q.peek();
+        }
+    }
+}
