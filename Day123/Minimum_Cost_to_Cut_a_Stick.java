93fb467997c6bf85dd5a96a82506fe52b4a3f06d
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-05-28 19:59:43 +0530

    Add files via upload

---
+++ Day123/Minimum_Cost_to_Cut_a_Stick.java
@@ -0,0 +1,28 @@
+package Day123;
+
+import java.util.*;
+
+public class Minimum_Cost_to_Cut_a_Stick {
+    class Solution {
+        public int minCost(int n, int[] cuts) {
+            List<Integer> A = new ArrayList<>();
+            for (int a : cuts) {
+                A.add(a);
+            }
+            A.add(0);
+            A.add(n);
+            Collections.sort(A);
+            int k = A.size();
+            int[][] dp = new int[k][k];
+            for (int d = 2; d < k; ++d) {
+                for (int i = 0; i < k - d; ++i) {
+                    dp[i][i + d] = 1000000000;
+                    for (int m = i + 1; m < i + d; ++m) {
+                        dp[i][i + d] = Math.min(dp[i][i + d], dp[i][m] + dp[m][i + d] + A.get(i + d) - A.get(i));
+                    }
+                }
+            }
+            return dp[0][k - 1];
+        }
+    }
+}
