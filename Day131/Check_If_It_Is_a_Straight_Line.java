07891aaa383147a0d40c3f764236ce94c49922bb
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-06-05 09:12:42 +0530

    Add files via upload

---
+++ Day131/Check_If_It_Is_a_Straight_Line.java
@@ -0,0 +1,29 @@
+package Day131;
+
+public class Check_If_It_Is_a_Straight_Line {
+    class Solution {
+        // Returns the delta Y.
+        int getYDiff(int[] a, int[] b) {
+            return a[1] - b[1];
+        }
+
+        // Returns the delta X.
+        int getXDiff(int[] a, int[] b) {
+            return a[0] - b[0];
+        }
+
+        public boolean checkStraightLine(int[][] coordinates) {
+            int deltaY = getYDiff(coordinates[1], coordinates[0]);
+            int deltaX = getXDiff(coordinates[1], coordinates[0]);
+
+            for (int i = 2; i < coordinates.length; i++) {
+                // Check if the slope between points 0 and i, is the same as between 0 and 1.
+                if (deltaY * getXDiff(coordinates[i], coordinates[0])
+                        != deltaX * getYDiff(coordinates[i], coordinates[0])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
