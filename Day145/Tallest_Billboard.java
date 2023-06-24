40ce6f2fd4bd2800992d030e25e1d895b3237751
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-06-24 22:07:42 +0530

    Add files via upload

---
+++ Day145/Tallest_Billboard.java
@@ -0,0 +1,21 @@
+package Day145;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tallest_Billboard {
+    class Solution {
+        public int tallestBillboard(int[] rods) {
+            Map<Integer, Integer> dp = new HashMap<>(), cur;
+            dp.put(0, 0);
+            for (int x : rods) {
+                cur = new HashMap<>(dp);
+                for (int d : cur.keySet()) {
+                    dp.put(d + x, Math.max(cur.get(d), dp.getOrDefault(x + d, 0)));
+                    dp.put(Math.abs(d - x), Math.max(cur.get(d) + Math.min(d, x), dp.getOrDefault(Math.abs(d - x), 0)));
+                }
+            }
+            return dp.get(0);
+        }
+    }
+}
