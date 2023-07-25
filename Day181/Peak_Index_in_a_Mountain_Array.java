e270d4b1828f010a4f7270c778c1a19c0750e70c
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-07-25 23:20:46 +0530

    Add files via upload

---
+++ Day181/Peak_Index_in_a_Mountain_Array.java
@@ -0,0 +1,23 @@
+package Day181;
+
+public class Peak_Index_in_a_Mountain_Array {
+    class Solution {
+        public int peakIndexInMountainArray(int[] arr) {
+            int start = 0;
+            int end = arr.length-1;
+
+            while(start<end){
+                int mid = start + (end - start)/2;
+
+                if(arr[mid]>arr[mid+1]){
+                    end = mid;
+                }else {
+                    start = mid +1;
+                }
+
+            }
+            return start;
+        }
+
+    }
+}
