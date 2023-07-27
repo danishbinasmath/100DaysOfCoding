627d4b45fcd5547d53cf9bb01258bbb3fe3aa217
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-07-27 19:00:07 +0530

    Add files via upload

---
+++ Day183/Maximum_Running_Time_of_N_Computers.java
@@ -0,0 +1,26 @@
+package Day183;
+
+public class Maximum_Running_Time_of_N_Computers {
+    class Solution {
+        public long maxRunTime(int n, int[] batteries) {
+            long sumPower = 0;
+            for (int power : batteries)
+                sumPower += power;
+            long left = 1, right = sumPower / n;
+
+            while (left < right){
+                long target = right - (right - left) / 2;
+                long extra = 0;
+
+                for (int power : batteries)
+                    extra += Math.min(power, target);
+
+                if (extra >= (long)(n * target))
+                    left = target;
+                else
+                    right = target - 1;
+            }
+            return left;
+        }
+    }
+}
