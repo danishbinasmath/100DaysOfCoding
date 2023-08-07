3cbeb9ae9a8da97f5d56a46766e276a597257945
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-08-07 23:14:20 +0530

    Update Search_a_2D_Matrix.java

--- Day194/Search_a_2D_Matrix.java
+++ Day194/Search_a_2D_Matrix.java
@@ -1,4 +1,27 @@
 package Day194;
 
 public class Search_a_2D_Matrix {
+  class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int low = 0;
+        int high = (m * n) - 1;
+
+        while(low <= high) {
+          int mid = (low + (high - low)/2);
+
+          if(matrix[mid/m][mid%m] == target){
+            return true;
+          }
+          if(matrix[mid/m][mid%m] > target) {
+            high = mid - 1;
+          }else {
+            low = mid + 1;
+          }
+        }
+      return false;
+    }
+  }
 }
