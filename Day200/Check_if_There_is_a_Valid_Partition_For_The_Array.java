e4ffb7bb1f32f7571c0343144961a45f2174baa5
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-08-13 21:12:11 +0530

    Add files via upload

---
+++ Day200/Check_if_There_is_a_Valid_Partition_For_The_Array.java
@@ -0,0 +1,19 @@
+package Day200;
+
+public class Check_if_There_is_a_Valid_Partition_For_The_Array {
+    class Solution {
+        public boolean validPartition(int[] A) {
+            int n = A.length, dp[] = new int[] {0, 0, 0, 1};
+            for (int i = 0; i < n; ++i) {
+                dp[i % 4] = 0;
+                if (i - 1 >= 0 && A[i] == A[i - 1])
+                    dp[i % 4] |= dp[(i + 2) % 4];
+                if (i - 2 >= 0 && A[i] == A[i - 1] && A[i - 1] == A[i - 2])
+                    dp[i % 4] |= dp[(i + 1) % 4];
+                if (i - 2 >= 0 && A[i] - 1 == A[i - 1] && A[i - 1] == A[i - 2] + 1)
+                    dp[i % 4] |= dp[(i + 1) % 4];
+            }
+            return dp[(n - 1) % 4] > 0;
+        }
+    }
+}
