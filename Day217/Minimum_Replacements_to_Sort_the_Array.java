79261ecda1e6101f0f3a21769bb1e1cb2e0d65e8
Muhammad Danish <102617200+danishbinasmath@users.noreply.github.com>
2023-08-30 19:19:59 +0530

    Add files via upload

---
+++ Day217/Minimum_Replacements_to_Sort_the_Array.java
@@ -0,0 +1,29 @@
+package Day217;
+
+public class Minimum_Replacements_to_Sort_the_Array {
+    class Solution {
+        public long minimumReplacement(int[] nums) {
+            long answer = 0;
+            int n = nums.length;
+
+            // Start from the second last element, as the last one is always sorted.
+            for (int i = n - 2; i >= 0; i--) {
+                // No need to break if they are already in order.
+                if (nums[i] <= nums[i + 1]) {
+                    continue;
+                }
+
+                // Count how many elements are made from breaking nums[i].
+                long numElements = (long)(nums[i] + nums[i + 1] - 1) / (long)nums[i + 1];
+
+                // It requires numElements - 1 replacement operations.
+                answer += numElements - 1;
+
+                // Maximize nums[i] after replacement.
+                nums[i] = nums[i] / (int)numElements;
+            }
+
+            return answer;
+        }
+    }
+}
