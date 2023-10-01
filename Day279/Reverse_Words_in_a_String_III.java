28a1b98fc4d1896e4a9c9062a1d0570a5383155c
Muhammad Danish <danishpk175@gmail.com>
2023-10-01 22:34:21 +0530

    Day279

---
+++ Day279/Reverse_Words_in_a_String_III.java
@@ -0,0 +1,26 @@
+package Day279;
+
+public class Reverse_Words_in_a_String_III {
+    public class Solution {
+        public String reverseWords(String s) {
+            char[] ca = s.toCharArray();
+            for (int i = 0; i < ca.length; i++) {
+                if (ca[i] != ' ') {   
+                    int j = i;
+                    while (j + 1 < ca.length && ca[j + 1] != ' ') { j++; } 
+                    reverse(ca, i, j);
+                    i = j;
+                }
+            }
+            return new String(ca);
+        }
+    
+        private void reverse(char[] ca, int i, int j) {
+            for (; i < j; i++, j--) {
+                char tmp = ca[i];
+                ca[i] = ca[j];
+                ca[j] = tmp;
+            }
+        }
+    }
+}
