97e9b34dfdd698858ac1a77449498e9570270c8d
Muhammad Danish <danishpk175@gmail.com>
2023-10-15 20:58:51 +0530

    Day293

---
+++ Day293/Number_of_Ways_to_Stay_in_the_Same_Place_After_Some_Steps.java
@@ -0,0 +1,34 @@
+package Day293;
+
+public class Number_of_Ways_to_Stay_in_the_Same_Place_After_Some_Steps {
+    class Solution {
+        public int numWays(int steps, int arrLen) {
+            int MOD = (int) 1e9 + 7;
+            arrLen = Math.min(arrLen, steps);
+            int[] dp = new int[arrLen];
+            int[] prevDp = new int[arrLen];
+            prevDp[0] = 1;
+            
+            for (int remain = 1; remain <= steps; remain++) {
+                dp = new int[arrLen];
+                
+                for (int curr = arrLen - 1; curr >= 0; curr--) {
+                    int ans = prevDp[curr];
+                    if (curr > 0) {
+                        ans = (ans + prevDp[curr - 1]) % MOD;
+                    }
+                    
+                    if (curr < arrLen - 1) {
+                        ans = (ans + prevDp[curr + 1]) % MOD;
+                    }
+                    
+                    dp[curr] = ans;
+                }
+                
+                prevDp = dp;
+            }
+            
+            return dp[0];
+        }
+    }
+}
