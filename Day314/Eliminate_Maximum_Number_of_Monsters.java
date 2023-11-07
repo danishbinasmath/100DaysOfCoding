a934e0aab932b5b101eb7dd61ddb7d3604370728
Muhammad Danish <danishpk175@gmail.com>
2023-11-07 20:48:13 +0530

    Create Eliminate_Maximum_Number_of_Monsters.java

---
+++ Day314/Eliminate_Maximum_Number_of_Monsters.java
@@ -0,0 +1,21 @@
+public class Eliminate_Maximum_Number_of_Monsters {
+    class Solution {
+        public int eliminateMaximum(int[] dist, int[] speed) {
+            PriorityQueue<Double> heap = new PriorityQueue();
+            for (int i = 0; i < dist.length; i++) {
+                heap.add ((double) dist[i] / speed[i]);
+            }
+            
+            int ans = 0;
+            while (!heap.isEmpty()) {
+                if (heap.remove() <= ans) {
+                    break;
+                }
+                
+                ans++;
+            }
+            
+            return ans;
+        }
+    }
+}
