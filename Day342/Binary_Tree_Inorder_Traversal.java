8869a35edd956c42b96a2aaeef1ba2d31dfdeda0
Muhammad Danish <danishpk175@gmail.com>
2023-12-09 23:24:15 +0530

    Create Binary_Tree_Inorder_Traversal.java

---
+++ Day342/Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,42 @@
+package Day342;
+
+import java.util.*;
+
+public class Binary_Tree_Inorder_Traversal {
+    
+ // Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if(root == null) {
+            return ans;
+        }
+
+        inorder(root, ans);
+        return ans;
+    }
+
+    public List<Integer> inorder(TreeNode root, List<Integer> ans) {
+        if(root != null) {
+            inorder(root.left, ans);
+            ans.add(root.val);
+            inorder(root.right, ans);
+        }
+        
+        return ans;
+    }
+}
+}
