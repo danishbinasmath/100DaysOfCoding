4fd52c1954c4e7e50dbd2a196442e753c7d76fa9
Muhammad Danish <danishpk175@gmail.com>
2023-12-26 21:07:57 +0530

    Day330

---
+++ Day330/Number_of_Dice_Rolls_With_Target_Sum.java
@@ -0,0 +1,33 @@
+package Day330;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Number_of_Dice_Rolls_With_Target_Sum {
+    class Solution {
+    int MOD = 1000000000 + 7;
+    Map<String, Integer> memo = new HashMap<>();
+    public int numRollsToTarget(int d, int f, int target) {
+        if (d == 0 && target == 0) {
+            return 1;
+        }
+        if (d == 0 || target == 0) {
+            return 0;
+        }
+        String str = d + " " + target;
+        if (memo.containsKey(str)) {
+            return memo.get(str);
+        }
+        int res = 0;
+        for (int i = 1; i <= f; i++) {
+            if (target >= i) {
+                res = (res + numRollsToTarget(d - 1, f, target - i)) % MOD;
+            } else {
+                break;
+            }
+        }
+        memo.put(str, res);
+        return res;
+    }
+}
+}
