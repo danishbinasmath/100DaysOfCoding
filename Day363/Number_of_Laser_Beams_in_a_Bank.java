140cbc31b3365b0f818d61cd3a1a28fc4ce70ef4
Muhammad Danish <danishpk175@gmail.com>
2024-01-03 21:27:37 +0530

    Create Number_of_Laser_Beams_in_a_Bank.java

---
+++ Day363/Number_of_Laser_Beams_in_a_Bank.java
@@ -0,0 +1,24 @@
+package Day363;
+
+public class Number_of_Laser_Beams_in_a_Bank {
+    class Solution {
+        public int numberOfBeams(String[] bank) {
+          int prev = 0, ans = 0;
+      
+          for (String s: bank) {
+            int count = 0;
+            for (int i = 0; i < s.length(); i++)
+              if (s.charAt(i) == '1') {
+                count++;
+              }
+      
+            if (count > 0) {
+              ans += prev * count;
+              prev = count;
+            }
+          }
+      
+          return ans;
+        }
+      }
+}
