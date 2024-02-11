e144be77f3c9434378a963784824288b0ef573d9
Muhammad Danish <danishpk175@gmail.com>
2024-02-11 23:35:58 +0530

    Day401

---
+++ Day400/Palindromic_Substrings.java
@@ -0,0 +1,36 @@
+package Day400;
+
+public class Palindromic_Substrings {
+    class Solution {
+        public int countSubstrings(String s) {
+            int n = s.length();
+            boolean[][] dp = new boolean[n][n];
+            int count = 0;
+    
+            for (int i = 0; i < n; i++) {
+                dp[i][i] = true;
+                count++;
+            }
+    
+            for (int i = 0; i < n - 1; i++) {
+                if (s.charAt(i) == s.charAt(i + 1)) {
+                    dp[i][i + 1] = true;
+                    count++;
+                }
+            }
+    
+            for (int len = 3; len <= n; len++) {
+                for (int i = 0; i < n - len + 1; i++) {
+                    int j = i + len - 1;
+                    if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                        count++;
+                    }
+                }
+            }
+    
+            return count;
+        }
+    }
+    
+}
