5396190d148406efa4a52ddf3baee23f23d9ea6a
Muhammad Danish <danishpk175@gmail.com>
2024-02-12 21:36:00 +0530

    Create Majority_Element.java

---
+++ Day402/Majority_Element.java
@@ -0,0 +1,28 @@
+package Day402;
+
+public class Majority_Element {
+    // Method 1: using sorting
+    // class Solution {
+    // public int majorityElement(int[] nums) {
+    // Arrays.sort(nums);
+    // return nums[nums.length/2];
+    // }
+    // }
+
+    // Method 2: using Boyer-Moore Voting Algorithm
+    class Solution {
+        public int majorityElement(int[] nums) {
+            int count = 0;
+            Integer candidate = null;
+
+            for (int num : nums) {
+                if (count == 0) {
+                    candidate = num;
+                }
+                count = count + ((num == candidate) ? 1 : -1);
+            }
+
+            return candidate;
+        }
+    }
+}
