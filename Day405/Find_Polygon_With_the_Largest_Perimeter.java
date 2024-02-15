d9e13000d24d84908d475e5aa2a4c0a2f08f772c
Muhammad Danish <danishpk175@gmail.com>
2024-02-15 20:32:03 +0530

    Create Find_Polygon_With_the_Largest_Perimeter.java

---
+++ Day405/Find_Polygon_With_the_Largest_Perimeter.java
@@ -0,0 +1,18 @@
+package Day405;
+
+public class Find_Polygon_With_the_Largest_Perimeter {
+    class Solution {
+        public long largestPerimeter(int[] nums) {
+            Arrays.sort(nums);
+            long previousElementsSum = 0;
+            long ans = -1;
+            for (int num : nums) {
+                if (num < previousElementsSum) {
+                    ans = num + previousElementsSum;
+                }
+                previousElementsSum += num;
+            }
+            return ans;
+        }
+    }
+}
