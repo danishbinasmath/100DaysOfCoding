cf3eb203f440734824b6b1f096aaa80afe7b8fc0
Muhammad Danish <danishpk175@gmail.com>
2024-02-22 16:01:23 +0530

    Create Find_the_Town_Judge.java

---
+++ Day412/Find_the_Town_Judge.java
@@ -0,0 +1,17 @@
+package Day412;
+
+public class Find_the_Town_Judge {
+    class Solution {
+        public int findJudge(int N, int[][] trust) {
+            int[] count = new int[N+1];
+            for (int[] t: trust) {
+                count[t[0]]--;
+                count[t[1]]++;
+            }
+            for (int i = 1; i <= N; ++i) {
+                if (count[i] == N - 1) return i;
+            }
+            return -1;
+        }
+    }
+}
