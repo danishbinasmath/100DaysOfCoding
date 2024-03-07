d9be0193d0d43c83b9fc2291bbe22ebf2fb7363e
Muhammad Danish <danishpk175@gmail.com>
2024-03-07 18:32:19 +0530

    Create Middle_of_the_Linked_List.java

---
+++ Day426/Middle_of_the_Linked_List.java
@@ -0,0 +1,28 @@
+package Day426;
+
+public class Middle_of_the_Linked_List {
+    // Definition for singly-linked list.
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+
+            return slow;
+        }
+    }
+}
