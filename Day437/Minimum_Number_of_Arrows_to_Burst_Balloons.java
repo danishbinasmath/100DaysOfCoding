22694c2724a4c8f1d6b3401b390fac58ad7b07fc
Muhammad Danish <danishpk175@gmail.com>
2024-03-18 20:27:52 +0530

    Create Minimum_Number_of_Arrows_to_Burst_Balloons.java

---
+++ Day437/Minimum_Number_of_Arrows_to_Burst_Balloons.java
@@ -0,0 +1,25 @@
+package Day437;
+
+import java.util.Arrays;
+
+public class Minimum_Number_of_Arrows_to_Burst_Balloons {
+    class Solution {
+        public int findMinArrowShots(int[][] points) {
+            if (points == null || points.length == 0) return 0;
+            
+            Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+            
+            int arrows = 1;
+            int end = points[0][1];
+            
+            for (int i = 1; i < points.length; i++) {
+                if (points[i][0] > end) {
+                    arrows++;
+                    end = points[i][1];
+                }
+            }
+            
+            return arrows;
+        }
+    }
+}
