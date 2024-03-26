f77e62c47ff4b4546cb85e417edfad7b9d0e3241
Muhammad Danish <danishpk175@gmail.com>
2024-03-26 23:17:22 +0530

    Create First_Missing_Positive.java

---
+++ Day445/First_Missing_Positive.java
@@ -0,0 +1,29 @@
+package Day445;
+
+public class First_Missing_Positive {
+    class Solution {
+        public int firstMissingPositive(int[] arr) {
+            int i = 0;
+            while(i<arr.length){
+                int correct = arr[i]-1;
+                if(arr[i]>0 && arr[i]<=arr.length && arr[i]!=arr[correct]){
+                    swap(arr,i,correct);
+                }else{
+                    i++;
+                }
+            }
+            for(int index=0; index<arr.length; index++){
+                if(arr[index]!=index+1){
+                    return index+1;
+                }
+            }
+            return arr.length+1;
+        }
+        
+        public void swap(int[] arr, int first, int second) {
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+    }
+}
