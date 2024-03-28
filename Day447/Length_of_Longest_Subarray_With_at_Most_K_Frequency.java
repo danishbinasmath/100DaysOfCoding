c177ef5ea9bcf310ea7c2bc95483bdf42e1888e6
Muhammad Danish <danishpk175@gmail.com>
2024-03-28 22:07:27 +0530

    Create Length_of_Longest_Subarray_With_at_Most_K_Frequency.java

---
+++ Day447/Length_of_Longest_Subarray_With_at_Most_K_Frequency.java
@@ -0,0 +1,27 @@
+package Day447;
+
+public class Length_of_Longest_Subarray_With_at_Most_K_Frequency {
+    class Solution {
+        public int maxSubarrayLength(int[] nums, int k) {
+            int n = nums.length;
+            Map<Integer, Integer> frequency = new HashMap();
+            int start = 0;
+            int charsWithFreqOverK = 0;
+            
+            for (int end = 0; end < n; end++) {
+                frequency.put(nums[end], frequency.getOrDefault(nums[end], 0) + 1);
+                if (frequency.get(nums[end]) == k + 1) { 
+                    charsWithFreqOverK++;
+                }
+                if (charsWithFreqOverK > 0) { 
+                    frequency.put(nums[start], frequency.get(nums[start]) - 1); 
+                    if (frequency.get(nums[start]) == k) { 
+                        charsWithFreqOverK--; 
+                    }
+                    start++;
+                }
+            }
+            return n - start; 
+        }
+    }
+}
