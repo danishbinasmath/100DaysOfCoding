2356a567a4054841ac4eddc0c2d6bf4e0b94c3bd
Muhammad Danish <danishpk175@gmail.com>
2024-04-06 22:38:07 +0530

    Create Minimum_Remove_to_Make_Valid_Parentheses.java

---
+++ Day456/Minimum_Remove_to_Make_Valid_Parentheses.java
@@ -0,0 +1,22 @@
+package Day456;
+
+import java.util.Stack;
+
+public class Minimum_Remove_to_Make_Valid_Parentheses {
+    class Solution {
+        public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < sb.length(); ++i) {
+            if (sb.charAt(i) == '(') st.add(i);
+            if (sb.charAt(i) == ')') {
+            if (!st.empty()) st.pop();
+            else sb.setCharAt(i, '*');
+            }
+        }
+        while (!st.empty())
+            sb.setCharAt(st.pop(), '*');
+        return sb.toString().replaceAll("\\*", "");
+        }
+    }
+}
