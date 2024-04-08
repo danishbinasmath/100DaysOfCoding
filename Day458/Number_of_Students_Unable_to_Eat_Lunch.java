74ecc096088d80e5fd9f4572dde45167f2dc93fc
Muhammad Danish <danishpk175@gmail.com>
2024-04-08 20:08:47 +0530

    Create Number_of_Students_Unable_to_Eat_Lunch.java

---
+++ Day458/Number_of_Students_Unable_to_Eat_Lunch.java
@@ -0,0 +1,39 @@
+package Day458;
+
+public class Number_of_Students_Unable_to_Eat_Lunch {
+    class Solution {
+        public int countStudents(int[] students, int[] sandwiches) {
+            int circleStudentCount = 0;
+            int squareStudentCount = 0;
+    
+            // Count the number of students who want each type of sandwich
+            for (int student : students) {
+                if (student == 0) {
+                    circleStudentCount++;
+                } else {
+                    squareStudentCount++;
+                }
+            }
+    
+            // Serve sandwiches to students
+            for (int sandwich : sandwiches) {
+                // No student wants the circle sandwich on top of the stack
+                if (sandwich == 0 && circleStudentCount == 0) {
+                    return squareStudentCount;
+                }
+                // No student wants the square sandwich on top of the stack
+                if (sandwich == 1 && squareStudentCount == 0) {
+                    return circleStudentCount;
+                }
+                // Decrement the count of the served sandwich type
+                if (sandwich == 0) {
+                    circleStudentCount--;
+                } else {
+                    squareStudentCount--;
+                }
+            }
+            // Every student received a sandwich
+            return 0;
+        }
+    }
+}
