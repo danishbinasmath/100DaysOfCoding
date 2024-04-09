83ad2d5e67a99a023ff03cb98ad68171d679fa45
Muhammad Danish <danishpk175@gmail.com>
2024-04-09 17:24:34 +0530

    Create Time_Needed_to_Buy_Tickets.java

---
+++ Day459/Time_Needed_to_Buy_Tickets.java
@@ -0,0 +1,23 @@
+package Day459;
+
+public class Time_Needed_to_Buy_Tickets {
+    class Solution {
+        public int timeRequiredToBuy(int[] tickets, int k) {
+            int time = 0;
+            
+            for (int i = 0; i < tickets.length; i++) {
+                // If the current person is before or at the desired person 'k'
+                if (i <= k) {
+                    // Buy the minimum of tickets available at person 'k' and the current person
+                    time += Math.min(tickets[k], tickets[i]);
+                } else {
+                    // If the current person is after 'k', buy the minimum of 
+                    // (tickets available at person 'k' - 1) and the current person
+                    time += Math.min(tickets[k] - 1, tickets[i]);
+                }
+            }
+            
+            return time;
+        }
+    }
+}
